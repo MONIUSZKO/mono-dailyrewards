605955b96e984b3644876ad5a31fe787c2c74d2a
M O N O <110067389+MONIUSZKO@users.noreply.github.com>
2023-08-21 23:22:46 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version  'cerulean'
+game        'gta5'
+
+author 'M O N O / T O X I C'
+description 'Prosty skrypt na daily reawrds'
+version '1.0.0'
+
+server_scripts{
+    'server/*.lua',
+    'config/*.lua',
+    'locales/pl.lua',
+    'locales/eng.lua'
+}
+
+shared_script 'config.lua'
+
+dependency 'es_extended'
