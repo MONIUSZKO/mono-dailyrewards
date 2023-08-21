d5fefa4137d2c3284777868dc37481738e5d938b
M O N O <110067389+MONIUSZKO@users.noreply.github.com>
2023-08-21 23:20:36 +0200

    Create pl.lua

---
+++ locales/pl.lua
@@ -0,0 +1,9 @@
+Locales['pl']       = {
+    ['taken_0']     = 'Odebrałeś już dziś nagrodę',
+    ['taken_1']     = 'Odebrales juz wszystkie nagrody',
+    ['taken_2']     = 'Odebrałeś swoją codzienną nagrodę! Dostajesz nagrodę numer ' .. numRewards ..,
+    ['taken_3']     = 'Odebrałeś swoją codzienną nagrodę! Dostajesz nagrodę numer 1',
+    ['taken_4']     = 'Wystąpił błąd podczas sprawdzania nagrody',
+
+    ['command_hel'] = 'Odbierz codzienna nagrode',
+}
