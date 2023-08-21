f42a5c7c6cc980ed97d862f3727ba23cc3339782
M O N O <110067389+MONIUSZKO@users.noreply.github.com>
2023-08-21 23:21:48 +0200

    Add files via upload

---
+++ locales/eng.lua
@@ -0,0 +1,9 @@
+Locales['eng']       = {
+    ['taken_0']     = 'You`ve already received your award today',
+    ['taken_1']     = 'You have already received all the awards',
+    ['taken_2']     = 'You have received your daily prize! You receive award number ' .. numRewards ..,
+    ['taken_3']     = 'You have received your daily prize! You receive award number 1',
+    ['taken_4']     = 'An error occurred while receiving an award',
+
+    ['command_hel'] = 'Receive a daily prize',
+}
